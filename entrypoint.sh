67df3aaad086e4a22d96187ea4ea90cfbe59c588
2767321434 <2767321434@qq.com>
2017-03-16 15:14:53 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
+echo "=> Setting a ${_word} password to the root user"
+echo "root:$PASS" | chpasswd
+
+echo "=> Done!"
+
+echo "========================================================================"
+echo "You can now connect to this container via SSH using:"
+echo ""
+echo "    ssh -p <port> root@<host>"
+echo "and enter the root password '$PASS' when prompted"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "========================================================================"
+
+nohup /usr/sbin/sshd -D &
