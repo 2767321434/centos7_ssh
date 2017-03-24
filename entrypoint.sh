98cf891538c41c8780e01a7988c7d45b9fd10a6f
2767321434 <2767321434@qq.com>
2017-03-24 12:25:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,11 @@ PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
 _word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
 echo "=> Setting a ${_word} password to the root user"
 echo "root:$PASS" | chpasswd
-echo ${SS_PASS} |./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+_sspw=${SS_PASS}
+if [ -z "$_sspw" ]; then 
+   _sspw="123456" 
+fi
+echo $_sspw |./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
 echo "=> Done!"
 
 echo "========================================================================"
@@ -12,7 +16,7 @@ echo "You can now connect to this container via SSH using:"
 echo ""
 echo "    ssh -p <port> root@<host>"
 echo "and enter the root password '$PASS' when prompted"
-echo ""
+echo "ss password is '$_sspw'"
 echo "Please remember to change the above password as soon as possible!"
 echo "========================================================================"
 
