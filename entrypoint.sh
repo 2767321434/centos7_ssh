e52ec28e00910d117a3090eb263442be4243e2c9
2767321434 <2767321434@qq.com>
2017-04-01 10:59:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
-_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
+PASS=${ROOT_PASS:-$(echo "123456")}
+_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "default" )
 echo "=> Setting a ${_word} password to the root user"
 echo "root:$PASS" | chpasswd
 _sspw=${SS_PASS}
 if [ -z "$_sspw" ]; then 
    _sspw="123456" 
 fi
-echo $_sspw |/usr/local/bin/shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+
 echo "=> Done!"
 
 echo "========================================================================"
@@ -21,3 +21,5 @@ echo "Please remember to change the above password as soon as possible!"
 echo "========================================================================"
 
 nohup /usr/sbin/sshd -D &
+
+echo $_sspw |/usr/local/bin/shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
